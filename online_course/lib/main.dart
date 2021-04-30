5a1d752370c7f112ac9346786e420ef58c28f224
Huỳnh Quốc Thịnh <quocthinh.huynh26@gmail.com>
2021-04-30 12:58:32 +0700

    Thêm package: flutter_rating_bar cho carousel_slider

--- online_course/lib/main.dart
+++ online_course/lib/main.dart
@@ -17,7 +17,7 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(initialRoute: '/root_screen', routes: {
+    return MaterialApp(initialRoute: '/home', routes: {
       '/': (context) => Welcome(),
       '/sign_in': (context) => SignIn(),
       '/helper': (context) => Helper(),
