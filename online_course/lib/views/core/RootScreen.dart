c74e1fa9d2ca3e466fcadd1cf55559e55dc66087
Huỳnh Quốc Thịnh <quocthinh.huynh26@gmail.com>
2021-05-14 18:13:22 +0700

    Tắt. animation default đối với selected và unselected bottom navigation item. Thêm các property tự kiểm soát animation đối vs từng item

--- online_course/lib/views/core/RootScreen.dart
+++ online_course/lib/views/core/RootScreen.dart
@@ -9,25 +9,26 @@ class RootScreen extends StatefulWidget {
 }
 
 class _RootScreenState extends State<RootScreen> {
-  int _currentIndex = 4;
+  int _currentIndex = 0;
 
   List<Widget> mainScreens = <Widget>[
     Home(),
     Search(),
-    Center(child: Text("DOWNLOAD")),
-    Center(child: Text("BROWSE")),
+    Center(child: Text("My Courses")),
+    Center(child: Text("Wishlist")),
     Account(),
   ];
 
   List<BottomNavigationBarItem> navigationBarIcons = <BottomNavigationBarItem>[
-    BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
-    BottomNavigationBarItem(icon: Icon(Icons.search), label: "Search"),
+    BottomNavigationBarItem(icon: Icon(Icons.star_border_rounded), label: "Home", activeIcon:  Icon(Icons.star_rounded)),
+    BottomNavigationBarItem(icon: Icon(Icons.search_outlined), label: "Search", activeIcon:  Icon(Icons.search_rounded)),
+
     BottomNavigationBarItem(
-        icon: Icon(Icons.stacked_bar_chart), label: "Browse"),
+        icon: Icon(Icons.play_circle_outline), label: "My Courses", activeIcon:  Icon(Icons.play_circle_fill)),
     BottomNavigationBarItem(
-        icon: Icon(Icons.download_done_sharp), label: "Downloads"),
+        icon: Icon(Icons.favorite_border_outlined), label: "Wishlist", activeIcon:  Icon(Icons.favorite)),
     BottomNavigationBarItem(
-        icon: Icon(Icons.account_circle_sharp), label: "Account")
+        icon: Icon(Icons.account_circle_outlined), label: "Account", activeIcon:  Icon(Icons.account_circle_rounded))
   ];
 
   @override
@@ -44,14 +45,24 @@ class _RootScreenState extends State<RootScreen> {
               textTheme: Theme.of(context)
                   .textTheme
                   .copyWith(caption: new TextStyle(color: Colors.yellow))),
-          child: new BottomNavigationBar(
-            items: navigationBarIcons,
-            backgroundColor: Colors.grey[800],
-            currentIndex: _currentIndex,
-            selectedItemColor: Colors.blueAccent,
-            onTap: (tappedIndex) => {
-              setState(() => {_currentIndex = tappedIndex})
-            },
+          child: SizedBox(
+            height: 90,
+            child: new BottomNavigationBar(
+              items: navigationBarIcons,
+              type: BottomNavigationBarType.fixed,
+              backgroundColor: Colors.grey[800],
+              currentIndex: _currentIndex,
+              selectedItemColor: Colors.blueAccent,
+              selectedIconTheme: IconThemeData(color: Colors.blueAccent),
+              selectedFontSize: 12,
+              unselectedItemColor: Colors.white,
+              unselectedIconTheme: IconThemeData(color: Colors.white),
+              unselectedFontSize: 12,
+              showUnselectedLabels: true,
+              onTap: (tappedIndex) => {
+                setState(() => {_currentIndex = tappedIndex})
+              },
+            ),
           )),
     );
   }
